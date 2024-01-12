e55aed3451edff51e6bd4ab2d6c003e95fde9f39
kunal-belekar <143201243+kunal-belekar@users.noreply.github.com>
2024-01-12 14:09:50 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,34 @@
-/**
+/** 
  * @param {string} s
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanNumerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const current = romanNumerals[s[i]];
+        const next = romanNumerals[s[i + 1]];
+
+        if (next && current < next) {
+            // If the next numeral is larger than the current one, subtract the current value.
+            result += next - current;
+            i++; // Skip the next numeral as it has been considered.
+        } else {
+            result += current;
+        }
+    }
+
+    return result;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
